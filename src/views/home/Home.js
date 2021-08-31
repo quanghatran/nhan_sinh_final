c80e7a7c33d24185af473fa479f21d2368b9cc89
shoppinh <mactrungkien2000@gmail.com>
2021-08-31 20:36:30 +0700

    done particle bg

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -5,13 +5,31 @@ import Banners from "./banners/Banners";
 import DemoService from "./demoService/DemoService";
 import DirectMeetInfo from "./directMeetInfo/DirectMeetInfo";
 import MeaningNumbers from "./meaningNumbers/MeaningNumbers";
-import Particles from "react-particles-js";
 import logo from "../../images/body-bg.jpg";
-import particlesConfig from "./particlesConfig.json";
+import Particles from "react-particles-js";
+import particlesConfig from "./particle-config";
 const AppHome = () => {
   return (
     <React.Fragment>
-      <Particles params={particlesConfig} />
+      <div>
+        <Particles
+          params={particlesConfig}
+          style={{
+            backgroundImage: `url(${logo})`,
+            height: "100%",
+            position: "fixed",
+            zIndex: -10,
+            top: 0,
+            left: 0,
+          }}
+        ></Particles>
+      </div>
+
+      <Header />
+      <Banners />
+      <DemoService />
+      <DirectMeetInfo />
+      <Footer />
     </React.Fragment>
   );
 };
