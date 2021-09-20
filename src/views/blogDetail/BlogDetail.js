6bbfc241a4266ea6d68af96b1e12773d31fea029
shoppinh <mactrungkien2000@gmail.com>
2021-09-20 08:38:40 +0700

    merge done

--- src/views/blogDetail/BlogDetail.js
+++ src/views/blogDetail/BlogDetail.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { useHistory, useParams } from 'react-router';
-import blogApi from '../../api/blogApi';
-import Header from '../../common/header/Header';
-import Footer from '../../common/newfooter/NewFooter';
-import Background from '../../common/background/Background';
+import React, { useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router";
+import blogApi from "../../api/blogApi";
+import Header from "../../common/header/Header";
+import Footer from "../../common/newfooter/NewFooter";
+import Background from "../../common/background/Background";
 import {
   Button,
   InputAdornment,
@@ -11,48 +11,48 @@ import {
   TextField,
   Typography,
   Container,
-} from '@material-ui/core';
-import { Grid } from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search';
-import ads from '../../images/ads.jpg';
-import { Link } from 'react-router-dom';
+} from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import ads from "../../images/ads.jpg";
+import { Link } from "react-router-dom";
 
 const useStyle = makeStyles((theme) => ({
   contain: {
-    margin: '100px auto',
+    margin: "100px auto",
   },
   title: {
-    textAlign: 'center',
-    color: '#fff',
+    textAlign: "center",
+    color: "#fff",
   },
   content: {
-    textAlign: 'center',
-    color: '#fff',
+    textAlign: "center",
+    color: "#fff",
   },
   aboutSection: {
-    color: '#fff',
-    marginTop: '20px',
+    color: "#fff",
+    marginTop: "20px",
 
-    [theme.breakpoints.down('sm')]: {
-      display: 'none',
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
     },
   },
   ads: {
-    width: '100%',
-    height: '70%',
+    width: "100%",
+    height: "70%",
   },
   stickyBar: {
-    position: 'sticky',
+    position: "sticky",
     top: 20,
   },
   root: {
-    '& .MuiOutlinedInput-notchedOutline': {
-      borderColor: 'orange !important',
+    "& .MuiOutlinedInput-notchedOutline": {
+      borderColor: "orange !important",
     },
   },
   readMore: {
-    color: '#fff',
-    margin: '10px auto 20px',
+    color: "#fff",
+    margin: "10px auto 20px",
   },
 }));
 const BlogDetail = () => {
@@ -68,6 +68,7 @@ const BlogDetail = () => {
       setBlog(res.data);
     };
     fetchBlogById();
+    window.scrollTo(0, 0);
   }, [id]);
   useEffect(() => {
     const params = {
@@ -78,19 +79,19 @@ const BlogDetail = () => {
       setPopularBlogs(res.data.data);
     };
     fetchPopularBlogs();
-  });
+  }, []);
   const viewPopularBlogDetail = (id) => {
     history.push(`/blog/${id}`);
   };
   const readMoreBlog = () => {
-    history.push('/blog');
+    history.push("/blog");
   };
   return (
     <div className={classes.contain}>
       <Header />
       <Background />
-      <Container maxWidths='lg'>
-        {' '}
+      <Container maxWidths="lg">
+        {" "}
         <Grid container>
           <Grid item md={8}>
             {blog && (
@@ -107,19 +108,19 @@ const BlogDetail = () => {
             <Grid item md={12}>
               <TextField
                 className={classes.searchField}
-                id='searchField'
-                color='primary'
+                id="searchField"
+                color="primary"
                 fullWidth
-                variant='outlined'
-                placeholder='Tìm Kiếm'
+                variant="outlined"
+                placeholder="Tìm Kiếm"
                 InputProps={{
                   endAdornment: (
-                    <InputAdornment position='end'>
-                      <SearchIcon style={{ color: '#fff' }} />
+                    <InputAdornment position="end">
+                      <SearchIcon style={{ color: "#fff" }} />
                     </InputAdornment>
                   ),
                   style: {
-                    color: '#fff',
+                    color: "#fff",
                   },
                   classes: {
                     root: classes.root,
@@ -132,22 +133,22 @@ const BlogDetail = () => {
 
             <Grid item container md={12} className={classes.aboutSection}>
               <Typography
-                variant='h4'
+                variant="h4"
                 gutterBottom
-                style={{ color: '#f69320' }}
+                style={{ color: "#f69320" }}
               >
                 Về chúng tôi
               </Typography>
-              <Typography variant='subtitle1'>
+              <Typography variant="subtitle1">
                 ‘’Minh triết nhân sinh’’ là một trong các dự án mang giá trị
                 tinh thần to lớn, với mong muốn được cống hiến các dòng giá trị
                 minh triết của chính cá nhân tích luỹ qua các trải nghiệm cá
                 nhân, nghiên cứu khoa học để tìm ra công cụ áp dụng vào việc cho
                 mọi người một định hướng minh triết nhất.
               </Typography>
-              <Grid item md={12} sm={12} xs={12} style={{ margin: '20px 0' }}>
-                <Button variant='contained' color='primary' fullWidth>
-                  <a href='/xem-online' style={{ color: '#fff' }}>
+              <Grid item md={12} sm={12} xs={12} style={{ margin: "20px 0" }}>
+                <Button variant="contained" color="primary" fullWidth>
+                  <a href="/xem-online" style={{ color: "#fff" }}>
                     Tra cứu ngay
                   </a>
                 </Button>
@@ -157,13 +158,13 @@ const BlogDetail = () => {
               item
               container
               md={12}
-              style={{ color: '#fff', marginTop: 20 }}
+              style={{ color: "#fff", marginTop: 20 }}
             >
               <Grid item md={12}>
                 <Typography
-                  variant='h4'
+                  variant="h4"
                   gutterBottom
-                  style={{ color: '#f69320' }}
+                  style={{ color: "#f69320" }}
                 >
                   Bài viết phổ biến
                 </Typography>
@@ -174,7 +175,7 @@ const BlogDetail = () => {
                     item
                     container
                     md={12}
-                    justifyContent='space-between'
+                    justifyContent="space-between"
                     style={{ marginBottom: 20 }}
                     onClick={(e) => {
                       viewPopularBlogDetail(popularBlog._id);
@@ -183,20 +184,20 @@ const BlogDetail = () => {
                     <Grid item md={4}>
                       <img
                         src={popularBlog.thumbnail}
-                        alt=''
-                        style={{ width: '100%', height: 80 }}
+                        alt=""
+                        style={{ width: "100%", height: 80 }}
                       />
                     </Grid>
                     <Grid item md={6}>
-                      <Typography variant='body1'>
+                      <Typography variant="body1">
                         {popularBlog.title}
                       </Typography>
                     </Grid>
                   </Grid>
                 ))}
               <Button
-                variant='contained'
-                color='primary'
+                variant="contained"
+                color="primary"
                 onClick={readMoreBlog}
                 className={classes.readMore}
               >
@@ -204,7 +205,7 @@ const BlogDetail = () => {
               </Button>
             </Grid>
             <Grid item md={12}>
-              <img src={ads} alt='' className={classes.ads} />
+              <img src={ads} alt="" className={classes.ads} />
             </Grid>
           </Grid>
         </Grid>
